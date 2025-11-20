bef8b7550105d9288242ee37784686ab3f24f454
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-20 17:34:24 -0300

    :style: Atualiza o estilo do arquivo conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,15 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
+
 class ExampleRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires("box2d/3.1.1")
+        # escolha uma versao:
+        # self.requires("box2d/3.1.1") # C PURO
+        self.requires("box2d/2.4.1")  # C++
 
     def layout(self):
         cmake_layout(self)
